550ece6172eef071a199ebcd43c105d1bbb1b6e7
jonbiro <jonnybrx@gmail.com>
2018-11-27 20:25:01 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).count
+    string_arr = self.split(/\.|\?|\!/)
+    string_arr.reject { |e| e== "" || e==[]}.count
   end
 end
